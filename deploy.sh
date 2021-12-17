7f7a071b3c3ba4662b4a7a83c8c35cd2eb9ee0fa
宮川信人 <miyagawa-san@MacBook-Pro.local>
2021-12-17 12:25:10 +0900

    improvement github actions 3

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+ssh ayaka@ayaka.work <<EOC
 sudo systemctl stop lavlusd
 cd ~/ServerApp/lavlus_server
 git pull
 npm install 
 sed -i "s/192.168.1.200/127.0.0.1/g" src/datasources/db.datasource.ts
 sudo systemctl start lavlusd
+EOC
